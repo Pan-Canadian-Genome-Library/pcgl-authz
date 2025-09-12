78dcd32a707f2a6108601ad9a5547211d8e2d563
Daisie Huang <daisieh@gmail.com>
2025-09-11 17:04:38 -0700

    make backup and restore on clean/run

--- make_backup.sh
+++ make_backup.sh
@@ -6,11 +6,10 @@ flask=$(docker ps -a --format "{{.Names}}" | grep flask_1 | awk '{print $1}')
 mkdir -p $(pwd)/tmp/vault/backup
 docker exec $vault vault auth disable approle/
 stop=$(docker stop $vault)
-zip=$(docker exec $flask bash -c "cd /vault; tar -cz data/ > backup.tar.gz")
+zip=$(docker exec -u root $flask bash -c "cd /vault; tar -cz data/ > backup.tar.gz")
 copy=$(docker cp $flask:/vault/backup.tar.gz $(pwd)/tmp/vault/backup/)
 
 cp $(pwd)/tmp/vault/keys.txt $(pwd)/tmp/vault/backup
-cp $(pwd)/tmp/vault/service_stores.txt $(pwd)/tmp/vault/backup
 pwd=$(pwd)
 cd $(pwd)/tmp/vault
 tar -cz backup > backup.tar.gz
