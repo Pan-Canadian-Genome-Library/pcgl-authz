69361c6532a3db0a53d4b07d1495b6e98fd75b23
v-rocheleau <victor.rocheleau@mcgill.ca>
2025-10-17 18:05:45 -0400

    refact: copy permissions engine files to dir that can be safely shared

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -4,16 +4,20 @@ set -Euo pipefail
 
 
 if [[ -f "/app/initial_setup" ]]; then
+
+    cp /app/permissions_engine /permissions_engine
+    chmod 777 /permissions_engine
+
     mkdir /app/data
     chmod 777 /app/data
 
     # set up our default values
-    sed -i s@PCGL_ADMIN_GROUP@$PCGL_ADMIN_GROUP@ /app/permissions_engine/calculate.rego
+    sed -i s@PCGL_ADMIN_GROUP@$PCGL_ADMIN_GROUP@ /permissions_engine/calculate.rego
 
     token=$(dd if=/dev/urandom bs=1 count=16 2>/dev/null | base64 | tr -d '\n\r+' | sed s/[^A-Za-z0-9]//g)
-    echo { \"opa_secret\": \"$token\" } > /app/permissions_engine/opa_secret.json
+    echo { \"opa_secret\": \"$token\" } > /permissions_engine/opa_secret.json
     # set up vault URL
-    sed -i s@VAULT_URL@$VAULT_URL@ /app/permissions_engine/vault.rego
+    sed -i s@VAULT_URL@$VAULT_URL@ /permissions_engine/vault.rego
 
     echo "initializing stores"
     python3 /app/initialize_vault_store.py
@@ -23,18 +27,19 @@ if [[ -f "/app/initial_setup" ]]; then
     else
         echo "!!!!!! INITIALIZATION FAILED, TRY AGAIN !!!!!!"
     fi
-else
-    sleep 10
-    # unseal vault
-    KEY=$(head -n 2 /app/config/keys.txt | tail -n 1)
-    echo '{ "key": "'$KEY'" }' > payload.json
-    curl --request POST --data @payload.json http://vault:8200/v1/sys/unseal
-    KEY=$(head -n 3 /app/config/keys.txt | tail -n 1)
-    echo '{ "key": "'$KEY'" }' > payload.json
-    curl --request POST --data @payload.json http://vault:8200/v1/sys/unseal
-    KEY=$(head -n 4 /app/config/keys.txt | tail -n 1)
-    echo '{ "key": "'$KEY'" }' > payload.json
-    curl --request POST --data @payload.json http://vault:8200/v1/sys/unseal
+# TODO: add this to a new "dev-entrypoint.sh" file for local dev
+# else
+#     sleep 10
+#     # unseal vault
+#     KEY=$(head -n 2 /app/config/keys.txt | tail -n 1)
+#     echo '{ "key": "'$KEY'" }' > payload.json
+#     curl --request POST --data @payload.json http://vault:8200/v1/sys/unseal
+#     KEY=$(head -n 3 /app/config/keys.txt | tail -n 1)
+#     echo '{ "key": "'$KEY'" }' > payload.json
+#     curl --request POST --data @payload.json http://vault:8200/v1/sys/unseal
+#     KEY=$(head -n 4 /app/config/keys.txt | tail -n 1)
+#     echo '{ "key": "'$KEY'" }' > payload.json
+#     curl --request POST --data @payload.json http://vault:8200/v1/sys/unseal
 fi
 
 # make sure that our vault stores have the latest values
