e10bbf6390fe31c307861b876f1186f4c283f36d
Daisie Huang <daisieh@gmail.com>
2026-02-19 20:45:01 -0800

    look for temp user

--- app/src/auth.py
+++ app/src/auth.py
@@ -766,6 +766,12 @@ def get_user_record(comanage_id=None, oidcsub=None, force=False, service=SERVICE
         for email in response.json()["EmailAddresses"]:
             if email["Mail"] not in emails and email["Verified"]:
                 emails.append({"address": email["Mail"], "type": email["Type"]})
+                # see if we have any DAC auths for this email address:
+                temp_user, status_code = get_service_store_secret(service, key=f"users/{email["Mail"]}")
+                if status_code == 200:
+                    for auth in temp_user["study_authorizations"]:
+                        user["study_authorizations"][auth] = temp_user["study_authorizations"][auth]
+                    delete_service_store_secret(service, key=f"users/{email["Mail"]}")
     user["emails"] = emails
 
     set_service_store_secret(service, key=f"users/{comanage_id}", value=json.dumps(user))
