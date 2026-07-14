632391cc16bacb804d9a58d34e6004dbad98cee7
Daisie Huang <daisieh@gmail.com>
2026-07-13 17:21:03 -0700

    try using env var for opa secret

--- app/setup.sh
+++ app/setup.sh
@@ -7,7 +7,7 @@ sed -i s@PCGL_ADMIN_GROUP@$PCGL_ADMIN_GROUP@ /permissions_engine/calculate.rego
 sed -i s@PCGL_DATA_ADMIN_GROUP@$PCGL_DATA_ADMIN_GROUP@ /permissions_engine/calculate.rego
 
 token=$(dd if=/dev/urandom bs=1 count=16 2>/dev/null | base64 | tr -d '\n\r+' | sed s/[^A-Za-z0-9]//g)
-echo { \"opa_secret\": \"$token\" } > /permissions_engine/opa_secret.json
+echo { \"opa_secret\": \"$OPA_SECRET\" } > /permissions_engine/opa_secret.json
 # set up vault URL and namespace
 sed -i s@VAULT_URL@$VAULT_URL@ /permissions_engine/vault.rego
 sed -i s@VAULT_NAMESPACE@$VAULT_NAMESPACE@ /permissions_engine/vault.rego
